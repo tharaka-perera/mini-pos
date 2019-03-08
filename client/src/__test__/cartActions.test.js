691591560940460354363e0115dd85b6ce1ae2a1
NuwanTharaka <crizwii@gmail.com>
2019-03-08 13:14:18 +0530

    Reducer tests partially done\n Deploying to Heroku

--- client/src/__test__/cartActions.test.js
+++ client/src/__test__/cartActions.test.js
@@ -105,78 +105,82 @@ describe("getCartItems cartActions", () => {
     const expectedActions = [
       { type: types.CART_ITEMS_LOADING },
       {
-        payload: [
-          {
-            _id: "5c6e3f2ccd583f2bd04e55cc",
-            itm: {
-              _id: "5c67bb90f004c93ecd7006bf",
-              name: "Coke",
-              productCode: 2,
-              price: 100,
-              description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis metus dolor, at mollis ",
-              availableCount: 500,
-              __v: 0
+        payload: {
+          _id: "5c6bf6cdd2104b6379fdf49c",
+          items: [
+            {
+              _id: "5c6e3f2ccd583f2bd04e55cc",
+              itm: {
+                _id: "5c67bb90f004c93ecd7006bf",
+                name: "Coke",
+                productCode: 2,
+                price: 100,
+                description:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis metus dolor, at mollis ",
+                availableCount: 500,
+                __v: 0
+              },
+              count: 1
             },
-            count: 1
-          },
-          {
-            _id: "5c6e3fbc461b152d009ba691",
-            itm: {
-              _id: "5c67bd07f004c93ecd7006c6",
-              name: "Ice Cream",
-              productCode: 9,
-              price: 120,
-              description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis metus dolor, at mollis ",
-              availableCount: 170,
-              __v: 0
+            {
+              _id: "5c6e3fbc461b152d009ba691",
+              itm: {
+                _id: "5c67bd07f004c93ecd7006c6",
+                name: "Ice Cream",
+                productCode: 9,
+                price: 120,
+                description:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis metus dolor, at mollis ",
+                availableCount: 170,
+                __v: 0
+              },
+              count: 1
             },
-            count: 1
-          },
-          {
-            _id: "5c6e5d167d83b649d32e3c0c",
-            itm: {
-              _id: "5c67bbc7f004c93ecd7006c0",
-              name: "Apple",
-              productCode: 3,
-              price: 80,
-              description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis metus dolor, at mollis ",
-              availableCount: 600,
-              __v: 0
+            {
+              _id: "5c6e5d167d83b649d32e3c0c",
+              itm: {
+                _id: "5c67bbc7f004c93ecd7006c0",
+                name: "Apple",
+                productCode: 3,
+                price: 80,
+                description:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis metus dolor, at mollis ",
+                availableCount: 600,
+                __v: 0
+              },
+              count: 4
             },
-            count: 4
-          },
-          {
-            _id: "5c6fce27dedd73364924ed67",
-            itm: {
-              _id: "5c67bc01f004c93ecd7006c2",
-              name: "Milk",
-              productCode: 5,
-              price: 200,
-              description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis metus dolor, at mollis ",
-              availableCount: 400,
-              __v: 0
+            {
+              _id: "5c6fce27dedd73364924ed67",
+              itm: {
+                _id: "5c67bc01f004c93ecd7006c2",
+                name: "Milk",
+                productCode: 5,
+                price: 200,
+                description:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis metus dolor, at mollis ",
+                availableCount: 400,
+                __v: 0
+              },
+              count: 4
             },
-            count: 4
-          },
-          {
-            _id: "5c7379de4316081c7de67208",
-            itm: {
-              _id: "5c67bc28f004c93ecd7006c3",
-              name: "Cheese",
-              productCode: 6,
-              price: 350,
-              description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis metus dolor, at mollis ",
-              availableCount: 70,
-              __v: 0
-            },
-            count: 1
-          }
-        ],
+            {
+              _id: "5c7379de4316081c7de67208",
+              itm: {
+                _id: "5c67bc28f004c93ecd7006c3",
+                name: "Cheese",
+                productCode: 6,
+                price: 350,
+                description:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis metus dolor, at mollis ",
+                availableCount: 70,
+                __v: 0
+              },
+              count: 1
+            }
+          ],
+          __v: 51
+        },
         type: types.GET_CART
       }
     ];
