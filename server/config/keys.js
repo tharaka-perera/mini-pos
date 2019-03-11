4dda05fe5616abf8d715a4aa838fc895a754f00c
NuwanTharaka <crizwii@gmail.com>
2019-03-11 16:29:34 +0530

    pushing to server

---
+++ server/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://tharaka:thara036@cluster0-uzqqb.mongodb.net/test?retryWrites=true",
+  jwtKEY: "mini-pos-sysco"
+};
---
+++ server/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const items = require("./routes/api/items");
+const cart = require("./routes/api/carts");
+const user = require("./routes/api/user");
+
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+//Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//cookie parser
+app.use(cookieParser());
+
+//DB config
+const DB = require("./config/keys").mongoURI;
+
+//connect to DB
+
+mongoose
+  .connect(DB)
+  .then(() => console.log("DB connected"))
+  .catch(err => console.log(err));
+
+//Use routes
+app.use("/api/items", items);
+app.use("/api/cart", cart);
+app.use("/api/user", user);
+
+//serve static asset if in production
+if (process.env.NODE_ENV === "production") {
+  //set the static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+module.exports = app;
